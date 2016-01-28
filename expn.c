79d198a09782ab2f877ce0bb82b9956289b451e4
Pascal Cuoq <pascal.cuoq@trust-in-soft.com>
2016-01-28 16:35:35 +0100

    small tweaks

--- expn.c
+++ expn.c
@@ -1,4 +1,6 @@
 /* Question 1: exponentiation. Compute a^n mod m where n is secret.
-   Assume  a < m  and  m*m < UINT_MAX.  */
+   Assume  a < m  and  m*m < UINT_MAX. 
+   Reusing results from previous exercises is allowed. */
 
 unsigned expn(unsigned a, unsigned n, unsigned m);
+
